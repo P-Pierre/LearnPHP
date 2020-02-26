55e60875ef16fc280f431ee782e3cc61f9097be4
P-Pierre <43763524+P-Pierre@users.noreply.github.com>
2020-02-26 21:46:07 +0100

    Add files via upload

---
+++ tests/info.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Mon titre</title>
+</head>
+<body>
+	<?php //phpinfo()
+		$age_de_moi = 23;
+		echo "j'ai $age_de_moi ans <br />";
+		echo 'j\'ai' . $age_de_moi . 'ans';
+
+
+		$coordonnees = array (
+    		'prenom' => 'François',
+    		'nom' => 'Dupont',
+    		'adresse' => '3 Rue du Paradis',
+    		'ville' => 'Marseille');
+
+		echo '<pre>';
+		print_r($coordonnees);
+		echo '</pre>';
+
+		if (array_key_exists('nom', $coordonnees))
+		{
+		    echo 'La clé "nom" se trouve dans les coordonnées !<br />';
+		}
+
+		if (array_key_exists('pays', $coordonnees))
+		{
+		    echo 'La clé "pays" se trouve dans les coordonnées !<br />';
+		}
+
+		if (in_array('Marseille', $coordonnees))
+		{
+		    echo 'La valeur "Marseille" se trouve dans les coordonnees !<br />';
+		}
+
+
+		$fruits = array ('Banane', 'Pomme', 'Poire', 'Cerise', 'Fraise', 'Framboise');
+
+		echo '<pre>';
+		print_r($fruits);
+		echo '</pre>';
+
+		$position = array_search('Fraise', $fruits);
+		echo '"Fraise" se trouve en position ' . $position . '<br />';
+
+		$position = array_search('pamplemousse', $fruits);
+
+		if ($position == NULL){
+			echo '"pamplemousse" non trouvé<br />';
+		} else{
+			echo '"pamplemousse" se trouve en position ' . $position;
+		}
+	?>
+</body>
+</html>
